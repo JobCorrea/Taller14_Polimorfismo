5dd9e64f6a1fec105bc26760673e902383512c0f
Job Daniel Correa <jobcorre0912@gmail.com>
2026-05-20 21:01:44 -0500

    Pruebas de objetos - Persona-Profesor-Estudiante

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
 public class Main{
     public static void main(String[] args) {
+
         Figura f = new Figura();
         Figura circulo = new Circulo(8);
         Figura rectangulo = new Rectangulo(8, 4);
         f.calcularArea();
         circulo.calcularArea();
         rectangulo.calcularArea();
+        System.out.println();
+
+        Persona persona = new Persona();
+        Persona estudiante = new Estudiante("Juan", "2024-001");
+        Persona profesor = new Profesor("Carlos", "POO");
+
+        persona.presentarse();
+        estudiante.presentarse();
+        profesor.presentarse();
         System.out.println();
     }
 }
