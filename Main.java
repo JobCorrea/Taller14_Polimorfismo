c8e2294d266ab61cdfa1e86c99f6ceeb721d777a
Job Daniel Correa <jobcorre0912@gmail.com>
2026-05-20 20:46:13 -0500

    Pruebas de objetos - Figura-Circulo-Rectangulo

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(String[] args) {
-        
+        Figura f = new Figura();
+        Figura circulo = new Circulo(8);
+        Figura rectangulo = new Rectangulo(8, 4);
+        f.calcularArea();
+        circulo.calcularArea();
+        rectangulo.calcularArea();
+        System.out.println();
     }
 }
